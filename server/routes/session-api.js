80c72f5fead96f6a6ea15640a4f641627e0899c6
Tony Henderson <th_rams94@hotmail.com>
2021-09-20 09:49:46 -0500

    Added: code comments

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -18,47 +18,46 @@ const config = require('../../config');
 
 
 /**
- * authenticate user
+ * POST Sign In
+ * Attempts to sign the user in given 'userName' & 'password' in req.body
+ * Sends the proper error messages back to the client
  */
  router.post('/signin', async (req, res) => {
   try {
     User.findOne({userName: req.body.userName}, (err, user) => {
-      // Error
+      // Server error
       if (err) {
         console.log(err);
 
+        // Send 500 Response: 'Internal Server Error'
         const authenticateError = new ErrorResponse(500, "Internal Server Error", err);
         return res.status(500).send(authenticateError.toObject());
       }
-      // Successful
+      // Query went through
       else {
         // No user found - 404
         if (!user) {
-          const noUserError = new ErrorResponse(404, "No user found", null);
+          const noUserError = new ErrorResponse(404, "This user was not found", null);
           return res.status(404).send(noUserError.toObject());
         }
-        console.log('--User--');
-        console.log(user);
-        console.log('--req.body.password--')
-        console.log(req.body.password);
-        console.log('--user.password--');
-        console.log(user.password);
 
         const isAuthenticated = bcrypt.compareSync(req.body.password, user.password);
 
-        // Password is invalid - 401
+        // Password is invalid
         if (!isAuthenticated) {
           const invalidPasswordResponse = new BaseResponse(401, "Invalid Password", null);
 
+          // Send 401 response & BaseResponse: 'Invalid Password'
           return res.status(401).send(invalidPasswordResponse.toObject());
         }
         // Password matches user
         else {
-          // Sign Token
+          // Sign jsonwebtoken
           var token = jwt.sign({ session_user: req.params.userName }, config.web.secret, {
             expiresIn: "24h"
           });
 
+          // Send 200 response & BaseResponse: 'Login Successful'
           const logInSuccessful = new BaseResponse(200, "Login Successful", { auth: true, userName: user.userName, token: token });
           return res.status(200).send(logInSuccessful.toObject());
         }
@@ -68,6 +67,7 @@ const config = require('../../config');
   catch(e) {
     console.log(e);
 
+    // Send 500 response & ErrorResponse: 'Internal Server Error'
     const authenticateCatchErrorResponse = new ErrorResponse(500, "Internal Server Error", e.message);
     return res.status(500).send(authenticateCatchErrorResponse.toObject());
   }
