8cdfd328ee519c034948b601bb831677905cd7f0
joneskj55 <joneskj55@gmail.com>
2021-09-27 17:51:43 -0500

    formatting

--- src/app/pages/server-error/server-error.component.ts
+++ src/app/pages/server-error/server-error.component.ts
@@ -3,18 +3,17 @@
  * Title: server-error.component.ts
  * Author: Fred Marble
  * Description: Creating the Security Error component.
- */import { Component, OnInit } from '@angular/core';
+ */
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-error',
   templateUrl: './server-error.component.html',
-  styleUrls: ['./server-error.component.css']
+  styleUrls: ['./server-error.component.css'],
 })
 export class ServerErrorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
