cef23bf1e8a25436e6da2d8065afd173fc95e235
joneskj55 <joneskj55@gmail.com>
2021-09-27 15:17:25 -0500

    organize app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,44 +10,55 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
-import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import {
   HttpClient,
   HttpClientModule,
   HTTP_INTERCEPTORS,
 } from '@angular/common/http';
+
+// Flex
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Forms
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { MatCardModule } from '@angular/material/card';
-import { DeleteRecordDialogComponent } from './shared/delete-record-dialog/delete-record-dialog.component';
-import { SecurityQuestionListComponent } from './pages/security-question-list/security-question-list.component';
+
+// Bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// PrimeNG
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+
+// Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { DeleteRecordDialogComponent } from './shared/delete-record-dialog/delete-record-dialog.component';
+import { SecurityQuestionListComponent } from './pages/security-question-list/security-question-list.component';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { UserCreateComponent } from './pages/user-create/user-create.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { SecurityQuestionDetailsComponent } from './pages/security-question-details/security-question-details.component';
 import { SecurityQuestionCreateComponent } from './pages/security-question-create/security-question-create.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ResetPasswordFormComponent } from './shared/forms/reset-password-form/reset-password-form.component';
 import { VerifySecurityQuestionsFormComponent } from './shared/forms/verify-security-questions-form/verify-security-questions-form.component';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
 import { VerifyUsernameFormComponent } from './shared/forms/verify-username-form/verify-username-form.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -99,7 +110,7 @@ import { ContactComponent } from './pages/contact/contact.component';
     MessagesModule,
     MessageModule,
     MatStepperModule,
-    MatSelectModule
+    MatSelectModule,
   ],
   providers: [
     {
