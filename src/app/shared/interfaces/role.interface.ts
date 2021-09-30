8fcd0cee598802fa0ddffc5e278d6bd7a65964b7
joneskj55 <joneskj55@gmail.com>
2021-09-30 10:56:10 -0500

    add role interface & role service

---
+++ src/app/shared/interfaces/role.interface.ts
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  role.interface.ts
+; Author: Professor Krasso
+; Modified by: Kevin Jones
+; Date: 30 Sep 2021
+; Description: Interface for Role object
+;===========================================
+*/
+
+export interface Role {
+  _id?: string;
+  text: string;
+}
